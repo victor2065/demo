da49225f5fc998fae42c5dd3ff436bb9359cb2c7
victor2065 <wenhualiu340@gmail.com>
2023-07-18 09:45:52 +0800

    Initial Commit

---
+++ demo01/demo01App.swift
@@ -0,0 +1,17 @@
+//
+//  demo01App.swift
+//  demo01
+//
+//  Created by 刘文华 on 2023/7/18.
+//
+
+import SwiftUI
+
+@main
+struct demo01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
